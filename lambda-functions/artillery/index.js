3ef4fe1722038783b8ada7c6c6dc20ab03452040
PapaBlack <call@papa.black>
2023-09-14 22:00:49 +0200

    efs as node_env

--- lambda-functions/artillery/index.js
+++ lambda-functions/artillery/index.js
@@ -1,18 +1,22 @@
 import { runShellParallel, runShell, runModule, chmod } from './tools.js';
 import fs from 'fs';
+
 const ArtilleryDirectory = '/mnt/efs/res/modules/artillery';
+const configFile = 'artillery-config.yml';
 
 export const handler = async (event, context) => {
   if (fs.existsSync(ArtilleryDirectory)) {
     console.log('Starting chmod')
     chmod(ArtilleryDirectory);
 
-    const artilleryPath = `${ArtilleryDirectory}/artillery/bin/run`;
-    const command = `NODE_PATH=${ArtilleryDirectory} ${artilleryPath} ${configPath}`;
+    const artilleryPath = `${ArtilleryDirectory}/.bin/artillery`;
+    const command = `NODE_PATH=${ArtilleryDirectory}`;
 
     // await runShell(command);
     console.log('Starting artillery')
-    await runShellParallel(command, context);
+    await runShell(command);
+
+    await runShellParallel(`npx artillery run ${configFile}`, context);
     return { success: true }
   }else{
     return { success: false, error: 'NO ACCESS TO EFS MODULES' }
