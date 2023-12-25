f633394f96a9a1521a21d4666a9d745eeb2a7a32
root <root@BlackSUPREME>
2023-12-25 11:56:45 +0100

    v1.9.4

--- _tools.js
+++ _tools.js
@@ -14,8 +14,50 @@ function findRootWorkspacePath(currentPath) {
   }
 
   return currentPath;
-  }
+}
+
+function linkWorkspaces(packageJsonPath, rootDir){
+  const package = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+
+  package.workspaces.forEach((workspace) => {
+      if(fs.existsSync(`${rootDir}/${workspace}/node_modules`)){
+          removeDirectory(`${rootDir}/${workspace}/node_modules`);
+      }
+  
+      createAndLogSymlink( `${rootDir}/node_modules`,`${rootDir}/${workspace}/node_modules`);
+  });
+}
+
+function createAndLogSymlink(target, pathForLink) {
+    // Ensure absolute paths
+    const absoluteTarget = path.resolve(target);
+    const absolutePathForLink = path.resolve(pathForLink);
+  
+    // Create the symlink
+    fs.symlink(absoluteTarget, absolutePathForLink, (err) => {
+      if (err) {
+        console.error("Error creating symlink:", err);
+        return;
+      }
+  
+      // Log success message
+      console.log(`Symlink created: ${absolutePathForLink} -> ${absoluteTarget}`);
+    });
+}
+
+function removeDirectory(dirPath) {
+  const absoluteDirPath = path.resolve(dirPath);
+
+  fs.rmSync(absoluteDirPath, { recursive: true, force: true }, (err) => {
+    if (err) {
+      console.error("Error removing directory:", err);
+      return;
+    }
+    console.log(`Directory removed: ${absoluteDirPath}`);
+  });
+}
 
 module.exports = {
-    findRootWorkspacePath
+    findRootWorkspacePath,
+    linkWorkspaces
 }
