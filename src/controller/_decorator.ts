daedd4f39b9e2daea8c617cf8dec0dd423af805c
Papa Black <jakpawlik@gmail.com>
2025-01-31 04:36:07 +0100

    v3.7.11

--- src/controller/_decorator.ts
+++ src/controller/_decorator.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+
 import { Controller as NestController, ControllerOptions as NestControllerOptions } from '@nestjs/common';
 import { BootstrapRegistry } from '../../nest/decorators/RWSConfigInjector';
 import { RWSHTTPRoutingEntry, IHTTProute, IPrefixedHTTProutes } from '../routing/routes';
@@ -8,22 +10,32 @@ function isPrefixedRoutes(entry: RWSHTTPRoutingEntry): entry is IPrefixedHTTProu
   return 'prefix' in entry && 'routes' in entry;
 }
 
+export type RWSControllerMetadata = {
+  name: string;
+}
+
+export const RWSCONTROLLER_METADATA_KEY = 'rws:controller';
+
 export function RWSControllerDecorator(
     controllerName: string,
   ): ClassDecorator {    
     
     const routes = BootstrapRegistry.getConfig().http_routes as RWSHTTPRoutingEntry[];
     
-    // Find the matching route configuration
-    let routeConfig: IHTTProute | undefined;
-    for (const entry of routes) {
-      if (isPrefixedRoutes(entry)) {
-        if((entry as IPrefixedHTTProutes).controllerName === controllerName){
-          const ControllerDecorator: ClassDecorator = NestController(entry.prefix);
-          return ControllerDecorator;
-        }       
+    return (target: any) => {
+      // Find the matching route configuration
+      let routeConfig: IHTTProute | undefined;
+      for (const entry of routes) {
+        if (isPrefixedRoutes(entry)) {
+          if((entry as IPrefixedHTTProutes).controllerName === controllerName){
+            const ControllerDecorator: ClassDecorator = NestController(entry.prefix);
+            Reflect.defineMetadata(RWSCONTROLLER_METADATA_KEY, { name: controllerName }, target);
+
+            return ControllerDecorator(target);
+          }       
+        }
       }
+    
+      throw new Error(`No controller "${controllerName}" defined in routes`);
     }
-  
-    throw new Error(`No controller "${controllerName}" defined in routes`);
 }
