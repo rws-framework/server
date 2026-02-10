b1b9a8a58c18e83ccaaad9d7036426b2d6cd81f0
Papa Black <jakpawlik@gmail.com>
2026-02-10 13:31:43 +0100

    v3.30.0
    Changed files:
    - src/gateways/_gateway.ts

--- src/gateways/_gateway.ts
+++ src/gateways/_gateway.ts
@@ -1,4 +1,4 @@
-import {ITheGateway} from '../types/ITheGateway';
+import { ITheGateway } from '../types/ITheGateway';
 import { Server, Socket } from 'socket.io';
 import { ModuleRef } from '@nestjs/core';
 
@@ -7,16 +7,16 @@ import {
     WebSocketServer,
     ConnectedSocket,
     SubscribeMessage,
-  } from '@nestjs/websockets';
+} from '@nestjs/websockets';
 
 import { RealtimePoint, RWSFillService, RWSJSONMessage } from '../index';
 
-import { BlackLogger, Injectable } from '../../nest'; 
-import {UtilsService, ConsoleService, AuthService } from '../index';
+import { BlackLogger, Injectable } from '../../nest';
+import { UtilsService, ConsoleService, AuthService } from '../index';
 import { ConfigService } from '@nestjs/config';
 import { RWSWebsocketRoutingService } from '../services/RWSWebsocketRoutingService';
 
-interface JSONMessage<T = unknown>{
+interface JSONMessage<T = unknown> {
     method: string;
     msg: T;
     user_id: string;
@@ -40,7 +40,7 @@ interface SocketWsResponse<T> extends BaseResponse<T> {
 
 @WebSocketGateway()
 @Injectable()
-export abstract class RWSGateway implements ITheGateway{
+export abstract class RWSGateway implements ITheGateway {
     private logger = new BlackLogger(this.constructor.name);
 
     @WebSocketServer() server: Server;
@@ -51,28 +51,28 @@ export abstract class RWSGateway implements ITheGateway{
 
     constructor(
         private readonly moduleRef: ModuleRef,
-        public appConfigService: ConfigService,        
+        public appConfigService: ConfigService,
         private wsRoutingService: RWSWebsocketRoutingService
-    ){
+    ) {
     }
 
-    protected setPortHandler(){
+    protected setPortHandler() {
         const port = this.appConfigService.get<number>('ws_port');
-        if(port){
-            this.port = port;            
-        }   
+        if (port) {
+            this.port = port;
+        }
     }
 
     onModuleInit() {
         const rwsFillService = this.moduleRef.get(RWSFillService, { strict: false });
-        rwsFillService.fillBaseServices(this);   
-        
-        this.setPortHandler();        
+        rwsFillService.fillBaseServices(this);
 
-        if(this.port){
+        this.setPortHandler();
+
+        if (this.port) {
             this.server.listen(this.port);
             this.setupGlobalEventHandlers();
-        }        
+        }
     }
 
     private async setupGlobalEventHandlers() {
@@ -80,8 +80,8 @@ export abstract class RWSGateway implements ITheGateway{
         this.server.on('connection', (socket: Socket) => {
             socket.onAny((eventName: string, ...args: any[]) => {
                 this.handleAnyEvent(socket, eventName, args);
-            }); 
-        });        
+            });
+        });
 
         // Łapie błędy na poziomie serwera
         this.server.on('error', (error) => {
@@ -90,137 +90,152 @@ export abstract class RWSGateway implements ITheGateway{
     }
 
     protected async handleAnyEvent(socket: Socket, eventName: string, args: any) {
-        
-        if(!args.length){
+
+        if (!args.length) {
             return;
         }
 
+        let parsedArgs: RWSJSONMessage = {
+            method: null,
+            msg: null,
+            user_id: null
+        };
+
+        if(Array.isArray(args)){
+            for (const argLine of args) {
+                parsedArgs = this.parseArgs(argLine);
+            }
+        }else{
+            parsedArgs = this.parseArgs(args);
+        }        
+        
+        const method = parsedArgs.method;
+        
+        const data = parsedArgs.msg;
+        const userId = parsedArgs.user_id;
+
+        const foundRtp = Array.from(this.wsRoutingService.getRealtimePoints()).find(point => point[0] === eventName && point[1].getGateway().constructor.name === this.constructor.name);
+
+        if (!foundRtp) {
+            this.logger.warn(`There is no Realtime Point for event "${eventName}" bound to Gateway "${this.constructor.name}"`);
+            return;
+        }
+
+        const [pointName, realtimePoint] = foundRtp;
+
+        const rtPointRoutes = realtimePoint.getRoutes();
+
+        if (rtPointRoutes.has(method)) {
+            const pointRoute = rtPointRoutes.get(method);
+
+            await pointRoute.handler.call(realtimePoint, parsedArgs, socket);
+        } else {
+            this.logger.warn(`There is no "${method}" method in "${pointName}" Realtime Point`);
+        }
+    }
+
+    private parseArgs(argLine: any): RWSJSONMessage 
+    {
         const parsedArgs: RWSJSONMessage = {
             method: null,
             msg: null,
             user_id: null
         };
 
-        for(const argLine of args){
-            try {
-                const parsedLine: RWSJSONMessage = JSON.parse(argLine);
+        try {
+     
+            const parsedLine: RWSJSONMessage = argLine;
 
-                if(parsedLine.method){
-                    parsedArgs.method = parsedLine.method;
-                }            
-    
-                if(parsedLine.msg){
-                    parsedArgs.msg = parsedLine.msg;
-                }            
-    
-                if(parsedLine.user_id){
-                    parsedArgs.user_id = parsedLine.user_id;
-                }           
-            } catch(e: Error | any){
-                console.error('Not RWS Message format: ', argLine);
-            }            
-        }
+            if (parsedLine.method) {
+                parsedArgs.method = parsedLine.method;
+            }
 
-        const method = parsedArgs.method;
-        const data = parsedArgs.msg;
-        const userId = parsedArgs.user_id; 
+            if (parsedLine.msg) {
+                parsedArgs.msg = parsedLine.msg;
+            }
 
-        
-        const foundRtp = Array.from(this.wsRoutingService.getRealtimePoints()).find(point => point[0] === eventName && point[1].getGateway().constructor.name === this.constructor.name);
+            if (parsedLine.user_id) {
+                parsedArgs.user_id = parsedLine.user_id;
+            }
 
-        if(!foundRtp){
-            this.logger.warn(`There is no Realtime Point for event "${eventName}" bound to Gateway "${this.constructor.name}"`);
-            return;
-        }
-
-        const [pointName, realtimePoint] = foundRtp;   
-
-        const rtPointRoutes = realtimePoint.getRoutes();
-
-        if(rtPointRoutes.has(method)){            
-            const pointRoute = rtPointRoutes.get(method);
-
-            await pointRoute.handler.call(realtimePoint, parsedArgs, socket);
-        }else{
-            this.logger.warn(`There is no "${method}" method in "${pointName}" Realtime Point`);
+            return parsedArgs;
+        } catch (e: Error | any) {
+            console.error('Not RWS Message format: ', argLine);
+            return {
+                method: null,
+                msg: null,
+                user_id: null
+            };
         }
     }
-    
+
 
     socket: Socket<any, any, any, any>;
 
-    getJson(input: string): any
-    {
+    getJson(input: string): any {
         return JSON.parse(input);
-    
+
     }
 
-    sendJson(input: object): string
-    {
+    sendJson(input: object): string {
         return JSON.stringify(input);
     }
 
-    
-    handleConnection(@ConnectedSocket() socket: Socket): void {      
-        this.logger.log('WSClient connected:', socket.id);        
+
+    handleConnection(@ConnectedSocket() socket: Socket): void {
+        this.logger.log('WSClient connected:', socket.id);
 
     }
-    
+
     handleDisconnect(@ConnectedSocket() socket: Socket): void {
         this.logger.log('Client disconnected:', socket.id);
     }
 
-    emitMessage<T>(eventName: string, method: string, socket: Socket, data?: T, success: boolean = true): void
-    {
+    emitMessage<T>(eventName: string, method: string, socket: Socket, data?: T, success: boolean = true): void {
         const payload: SocketWsResponse<T> = { success, eventName, method, data: null };
 
-        if(data){
+        if (data) {
             payload.data = data;
         }
 
-        socket.emit(eventName, this.sendJson(payload));           
+        socket.emit(eventName, this.sendJson(payload));
     }
 
-    broadcastMessage<T>(eventName: string, method: string, data?: T, success: boolean = true): void
-    {
+    broadcastMessage<T>(eventName: string, method: string, data?: T, success: boolean = true): void {
         const payload: SocketWsResponse<T> = { success, eventName, method, data: null };
 
-        if(data){
+        if (data) {
             payload.data = data;
         }
 
         this.server.emit(eventName, this.sendJson(payload));
     }
 
-    broadcastToRoom<T>(room: string, eventName: string, method: string, data?: T, success: boolean = true): void
-    {
+    broadcastToRoom<T>(room: string, eventName: string, method: string, data?: T, success: boolean = true): void {
         const payload: SocketWsResponse<T> = { success, eventName, method, data: null };
 
-        if(data){
+        if (data) {
             payload.data = data;
         }
 
         this.server.to(room).emit(eventName, this.sendJson(payload));
     }
 
-    broadcastExceptSocket<T>(socket: Socket, eventName: string, method: string, data?: T, success: boolean = true): void
-    {
+    broadcastExceptSocket<T>(socket: Socket, eventName: string, method: string, data?: T, success: boolean = true): void {
         const payload: SocketWsResponse<T> = { success, eventName, method, data: null };
 
-        if(data){
+        if (data) {
             payload.data = data;
         }
 
         socket.broadcast.emit(eventName, this.sendJson(payload));
     }
 
-    getData<T>(input: string): T
-    {
+    getData<T>(input: string): T {
         return this.getJson(input).msg as T
     }
 
-    throwError(method: string, socket: Socket, error: Error | any): void
-    {        
+    throwError(method: string, socket: Socket, error: Error | any): void {
         console.error(JSON.parse(JSON.stringify(error, Object.getOwnPropertyNames(error))));
 
         socket.emit(method, this.sendJson({
@@ -230,34 +245,34 @@ export abstract class RWSGateway implements ITheGateway{
     }
 
     addMessageHandler(eventName: string, handlerName: string, serviceInstance: RealtimePoint) {
-        const _self = this;            
-        
+        const _self = this;
+
         const handlerFunction = function (client: Socket, payload: any) {
-          if (typeof (serviceInstance as any)[eventName] === 'function') {
-            return (serviceInstance as any)[eventName].call(serviceInstance, client, payload);
-          }
-    
-          _self.logger.warn(`There is no ${eventName} realtime route in "${serviceInstance.constructor.name}"`);
-    
-          return {
-            event: `${eventName}_error`,
-            data: { status: 'method_not_found' }
-          };
+            if (typeof (serviceInstance as any)[eventName] === 'function') {
+                return (serviceInstance as any)[eventName].call(serviceInstance, client, payload);
+            }
+
+            _self.logger.warn(`There is no ${eventName} realtime route in "${serviceInstance.constructor.name}"`);
+
+            return {
+                event: `${eventName}_error`,
+                data: { status: 'method_not_found' }
+            };
         }.bind(serviceInstance);
-        
+
         SubscribeMessage(eventName)(
-          this.constructor,
-          handlerName,
-          {
-            value: handlerFunction,
-            enumerable: true,
-            configurable: true,
-            writable: true
-          }
+            this.constructor,
+            handlerName,
+            {
+                value: handlerFunction,
+                enumerable: true,
+                configurable: true,
+                writable: true
+            }
         );
-    
+
         this.logger.debug(`Registered handler ${eventName} -> ${serviceInstance.constructor.name}`);
     }
 }
 
-export {JSONMessage, BaseResponse as BaseWsResponse, ErrorResponse as ErrorWsResponse, SocketWsResponse};
+export { JSONMessage, BaseResponse as BaseWsResponse, ErrorResponse as ErrorWsResponse, SocketWsResponse };
